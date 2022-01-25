5f7ef7effd1be07107a8f45790a9cfa43c190961
Nicholas Garcia <87832673+nagarci7@users.noreply.github.com>
2022-01-25 16:11:54 -0600

    working with the DOM

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+/**
+ * Create a Backpack object.
+ */
+
 const backpack = {
   name: "Everyday Backpack",
   volume: 30,
@@ -20,8 +24,8 @@ const backpack = {
 console.log("The backpack object:", backpack);
 console.log("The pocketNum value:", backpack.pocketNum);
 
-console.log('left before:', backpack.strapLength.left);
+console.log("left before:", backpack.strapLength.left);
 
-backpack.newStrapLength(10,15);
+backpack.newStrapLength(10, 15);
 
-console.log('left after:', backpack.strapLength.left);
+console.log("left after:", backpack.strapLength.left);
