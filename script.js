bc49551d719b46e743cbe34645e06a2b6c630e54
Nicholas Garcia <87832673+nagarci7@users.noreply.github.com>
2022-02-15 16:21:22 -0600

    update for assignment

--- script.js
+++ script.js
@@ -1,27 +1,59 @@
 /**
- * A standard function
- * @link https://developer.mozilla.org/en-US/docs/Glossary/Function
+ * Practice: Pass values between functions
+ *
+ * Create two functions:
+ * - Main function
+ *  - Creates new <article> element
+ *  - Populates <article> with content (see const content below)
+ *  - Returns <article> element to where function is called
+ * - Helper image function
+ *  - Creates new <figure> element
+ *  - Adds <img> markup pointing to frogpack.image
+ *  - Adds <figcaption> element with image description
+ *  - Returns <figure> element to where function is called
  */
 
-const greenPack = {
-  name: "Frog Pack",
-  color: "green",
+const frogpack = {
+  name: "Frog Backpack",
   volume: 8,
+  color: "green",
   pocketNum: 3,
+  strapLength: {
+    left: 10,
+    right: 10,
+  },
+  lidOpen: false,
+  image: "../../assets/images/frog.svg",
+  toggleLid: function (lidStatus) {
+    this.lidOpen = lidStatus;
+  },
+  newStrapLength: function (lengthLeft, lengthRight) {
+    this.strapLength.left = lengthLeft;
+    this.strapLength.right = lengthRight;
+  },
 };
 
-const addPack = function (currentPack) {
-  const newArticle = document.createElement("article");
-  newArticle.innerHTML = `
-    <h1>${currentPack.name}</h1>
-    <ul>
-      <li>Volume: ${currentPack.volume}</li>
-      <li>Color: ${currentPack.color}</li>
-      <li>Number of pockets: ${currentPack.pocketNum}</li>
-    </ul>
-  `;
-  return newArticle;
-};
-
-const main = document.querySelector("main");
-main.append(addPack(greenPack));
+// Baseline HTML output
+const content = `
+    <h1 class="backpack__name">${frogpack.name}</h1>
+    <ul class="backpack__features">
+      <li class="packprop backpack__volume">Volume:<span> ${
+        frogpack.volume
+      }l</span></li>
+      <li class="packprop backpack__color">Color:<span> ${
+        frogpack.color
+      }</span></li>
+      <li class="packprop backpack__pockets">Number of pockets:<span> ${
+        frogpack.pocketNum
+      }</span></li>
+      <li class="packprop backpack__strap">Left strap length:<span> ${
+        frogpack.strapLength.left
+      } inches</span></li>
+      <li class="packprop backpack__strap">Right strap length:<span> ${
+        frogpack.strapLength.right
+      } inches</span></li>
+      <li class="feature backpack__lid">Lid status:<span> ${
+        frogpack.lidOpen ? "open" : "closed"
+      }</span></li>
+    </ul>  
+`;
